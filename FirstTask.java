f0128012c4df1188c526294b4f7e5afc467ce73d
boryamur <129318027+boryamur@users.noreply.github.com>
2023-03-29 20:34:42 +0300

    Create FirstTask.java

---
+++ FirstTask.java
@@ -0,0 +1,9 @@
+public class FirstTask {
+    int summa = 0;
+    public int sum (int number){
+        for (int i = 1; i <= number ; i++) {
+            summa+=i;
+        }
+        return summa;
+    } 
+}
